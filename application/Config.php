c84e13cb5e315321b0fb979c9c82a231ac49c712
adellemus <adellemusb2@gmail.com>
2022-02-17 19:23:58 -0300

    aplicacion

---
+++ application/Config.php
@@ -0,0 +1,17 @@
+<?php
+define('BASE_URL', 'http://localhost/carga/');
+define('DEFAULT_CONTROLLER', 'login');
+define('DEFAULT_LAYOUT', 'basic');
+define('APP_NAME', 'Soporte');
+define('APP_SLOGAN', 'Soporte');
+define('APP_COMPANY', 'COTEDEM');
+define('APP_TLF', '');
+define('APP_EMAIL', '');
+define('SESSION_TIME', 1000000);
+define('HASH_KEY', '4f6a6d832be79');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'carga');
+define('DB_CHAR', 'utf8');
+?>
