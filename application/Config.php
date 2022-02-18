cc8d7455917099d345210e104c23e185306ee989
adellemus <adellemusb2@gmail.com>
2022-02-17 21:33:28 -0300

    cambio en el usuario de la bd

--- application/Config.php
+++ application/Config.php
@@ -10,8 +10,8 @@ define('APP_EMAIL', '');
 define('SESSION_TIME', 1000000);
 define('HASH_KEY', '4f6a6d832be79');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'dispati2');
-define('DB_PASS', '');
-define('DB_NAME', 'dfn');
+define('DB_USER', 'dispati2_dfnbd');
+define('DB_PASS', 'Beadel19.');
+define('DB_NAME', 'dispati2_dfn');
 define('DB_CHAR', 'utf8');
 ?>
